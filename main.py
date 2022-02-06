b2989524b48c5f1c960c1636f34594ba0cccb2bf
Andre Yanny <andre_osama@yahoo.com>
2022-02-06 16:58:02 +0200

    database v0.8

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sqlite3
 import os
+import games as g
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "gamender.db")
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
+
+print(g.get_top_game(2))
