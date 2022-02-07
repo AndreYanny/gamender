c3df5916468d9b6cbb09217105be1b36aa78853b
Andre Yanny <andre_osama@yahoo.com>
2022-02-07 02:02:46 +0200

    user interface v0.3

--- ui.py
+++ ui.py
@@ -17,7 +17,7 @@ def register():
     global register_screen
     register_screen = Toplevel(main_screen)
     register_screen.title("Register")
-    register_screen.geometry("300x250")
+    register_screen.geometry("300x400")
 
     global username
     global password
@@ -26,18 +26,34 @@ def register():
     username = StringVar()
     password = StringVar()
 
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="Please enter your details below", pady=5).pack()
     Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
+    username_lable = Label(register_screen, text="Username:")
     username_lable.pack()
     username_entry = Entry(register_screen, textvariable=username)
     username_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
+    password_lable = Label(register_screen, text="Password:")
     password_lable.pack()
     password_entry = Entry(register_screen, textvariable=password, show='*')
     password_entry.pack()
     Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="blue", command=register_user).pack()
+
+    CheckVar1 = IntVar()
+    CheckVar2 = IntVar()
+    CheckVar3 = IntVar()
+    CheckVar4 = IntVar()
+    CheckVar5 = IntVar()
+    CheckVar6 = IntVar()
+
+    Checkbutton(register_screen, text="Action", variable=CheckVar1, onvalue=1, offvalue=0, width=20).pack()
+    Checkbutton(register_screen, text="Adventure", variable=CheckVar2, onvalue=1, offvalue=0, width=20).pack()
+    Checkbutton(register_screen, text="RPG", variable=CheckVar3, onvalue=1, offvalue=0, width=20).pack()
+    Checkbutton(register_screen, text="Simulation", variable=CheckVar4, onvalue=1, offvalue=0, width=20).pack()
+    Checkbutton(register_screen, text="Strategy", variable=CheckVar5, onvalue=1, offvalue=0, width=20).pack()
+    Checkbutton(register_screen, text="Sports & Racing", variable=CheckVar6, onvalue=1, offvalue=0, width=20).pack()
+
+    Label(register_screen, text='').pack()
+    Button(register_screen, text="Register", width=10, height=1, command=register_user).pack()
 
 
 # Designing window for login 
@@ -47,7 +63,7 @@ def login():
     login_screen = Toplevel(main_screen)
     login_screen.title("Login")
     login_screen.geometry("300x250")
-    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="Please enter your details below to login", pady=5).pack()
     Label(login_screen, text="").pack()
 
     global username_verify
@@ -76,15 +92,12 @@ def register_user():
     username_info = username.get()
     password_info = password.get()
 
-    file = open(username_info, "w")
-    file.write(username_info + "\n")
-    file.write(password_info)
-    file.close()
+    u.add_user(username_info, password_info)
 
     username_entry.delete(0, END)
     password_entry.delete(0, END)
 
-    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+    Label(register_screen, text="Registration Success", fg="green", font=11).pack()
 
 
 # Implementing event on login button
